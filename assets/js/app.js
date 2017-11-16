e38b0f2664abff6fa0c3d6f5a38be8c701d196df
DianaChuctayaPach <leydiana.ch2015@gmail.com>
2017-11-16 12:02:10 -0500

    avance lab

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+function initMap() {
+        var laboratoriaLima = { lat: -12.1191427, lng: -77.0349046 };
+    
+            var map = new google.maps.Map(document.getElementById('map'), {
+       zoom: 15,
+                  center: laboratoriaLima
+                });
+    var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      } 
